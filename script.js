7007bc3340a6ee52b225ddf9d930d81e03284d96
mwahk3ymer <audreymwakima99@gmail.com>
2023-05-11 11:03:11 +0300

    toggle changes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const body = document.querySelector("body"),
-      sidebar  = body.querySelector("sidebar"),
-      toggle  = body.querySelector("toggle"),
-      searchBtn  = body.querySelector(".seach-box"),
+      sidebar  = body.querySelector(".sidebar"),
+      toggle  = body.querySelector(".toggle"),
+      searchBtn  = body.querySelector(".search-box"),
       modeSwitch  = body.querySelector(".toggle-switch"),
       modeText  = body.querySelector(".mode-text"),
 
       toggle.addEventListener("click", () =>{
-        sidebar.classList.toggle("dark");
+        sidebar.classList.toggle("close");
       });
 
        searchBtn.addEventListener("click", () =>{
-        sidebar.classList.toggle("dark");
+        sidebar.classList.remove("close");
       });
 
       modeSwitch.addEventListener("click", () =>{
